5f9f7f702e795fb5e9ae8a6befca83fd4daf3a9f
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-13 17:08:41 +0200

    [268] [Time Beats: 100.00%] [Memory Beats: 37.87%] - LeetPush

--- patterns-roadmap/Missing-Number.java
+++ patterns-roadmap/Missing-Number.java
@@ -1,12 +1,16 @@
 1class Solution {
 2    public int missingNumber(int[] nums) {
 3        
-4        int sum = 0;
-5        for(int i = 0; i < nums.length; i++) {
-6            sum += nums[i];
+4        int sum = nums[0];
+5        for(int i = 1; i < nums.length; i++) {
+6            sum ^= nums[i];
 7        }
-8        int n = nums.length;
-9        
-10        return (n*(n+1)/2) - sum;
-11    }
-12}
+8
+9        int real = 0;
+10        for(int i = 0; i <= nums.length; i++) {
+11            real ^= i;
+12        }
+13
+14        return sum ^ real;
+15    }
+16}
