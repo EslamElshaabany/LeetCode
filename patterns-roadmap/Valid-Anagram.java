19da9ed09a38efb4a94733bd71142aaa6bd6ed79
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-17 23:24:27 +0200

    [242] [Time Beats: 27.58%] [Memory Beats: 47.85%] - LeetPush

--- patterns-roadmap/Valid-Anagram.java
+++ patterns-roadmap/Valid-Anagram.java
@@ -1,15 +1,28 @@
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
 3
-4        char[] sArr = s.toCharArray();
-5        char[] tArr = t.toCharArray();
-6        
-7        Arrays.sort(sArr);
-8        Arrays.sort(tArr);
-9
-10        String sortedS = new String(sArr);
-11        String sortedT = new String(tArr);
-12
-13        return sortedS.equals(sortedT);
-14    }
-15}
+4        Map<Character, Integer> sMap = new HashMap<>();
+5        Map<Character, Integer> tMap = new HashMap<>();
+6
+7        for(char sChar: s.toCharArray()) {
+8            sMap.put(sChar, sMap.getOrDefault(sChar, 0) + 1);
+9        }
+10
+11        for(char tChar: t.toCharArray()) {
+12            tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
+13        }
+14
+15        return sMap.equals(tMap);
+16    }
+17}
+18
+19// solution 1: sorting o(nlogn)
+20// - convet to char array o(n)
+21// - sort the array o(nlogn)
+22// - new string form array o(n)
+23// - compare the strings o(n)
+24
+25// solution 2: map !o(o)
+26// insert elments in map o(n)
+27// compare maps o(n)
+28
