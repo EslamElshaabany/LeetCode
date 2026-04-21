660faa3b9b9369077415d09856f4d77091682c2c
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-21 16:27:33 +0200

    [643] [Time Beats: 50.46%] [Memory Beats: 65.16%] - LeetPush

--- patterns-roadmap/Maximum-Average-Subarray-I.java
+++ patterns-roadmap/Maximum-Average-Subarray-I.java
@@ -3,28 +3,29 @@
 3
 4        int numsSize = nums.length;
 5
-6        Double bestSum = 0.0;
+6        double bestSum = 0.0;
 7        for (int i = 0; i < k; i++) {
 8            bestSum += nums[i];  
 9        }
-10        Double currentWindow = bestSum;
-11        for (int i = k; i < numsSize; i++) {
-12            currentWindow += nums[i] - nums[i-k];
-13            bestSum =  Double.compare(currentWindow, bestSum) > 0 ? currentWindow : bestSum;
-14        }
-15        
-16        return bestSum / k;
-17    }
-18}
-19
-20// solution 1: o(n)
-21// loop on the array 
-22// add the current elemnt to sum array
-23// start form k as i and calc sum of k as sum[i] - sum[i-k]
-24// return max sum / k
-25
-26//[1,13,8,2,52,55]
-27// 2
-28// 51
-29// 42
-30
+10        
+11        double currentWindow = bestSum;
+12        for (int i = k; i < numsSize; i++) {
+13            currentWindow += nums[i] - nums[i-k];
+14            bestSum = Math.max(currentWindow, bestSum);
+15        }
+16        
+17        return bestSum / k;
+18    }
+19}
+20
+21// solution 1: o(n)
+22// loop on the array 
+23// add the current elemnt to sum array
+24// start form k as i and calc sum of k as sum[i] - sum[i-k]
+25// return max sum / k
+26
+27//[1,13,8,2,52,55]
+28// 2
+29// 51
+30// 42
+31
