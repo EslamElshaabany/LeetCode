d287787c68cd0bbfe136d05b73c03c7c0039c129
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-21 17:10:40 +0200

    [125] [Time Beats: 48.45%] [Memory Beats: 85.44%] - LeetPush

--- patterns-roadmap/Valid-Palindrome.java
+++ patterns-roadmap/Valid-Palindrome.java
@@ -1,36 +1,36 @@
 1class Solution {
 2    public boolean isPalindrome(String s) {
 3
-4        Deque<Character> stack = new ArrayDeque<>();
+4        String ss = sanitize(s);
 5
-6        String ss = sanitize(s);
+6        int start = 0, end = ss.length() - 1;
 7
-8        for (char c : ss.toCharArray()) {
-9            stack.push(c);
-10        }
-11
-12        for (char c : ss.toCharArray()) {
-13            if (stack.pop() != c) {
-14                return false;
-15            }
-16        }
-17
-18        return true;
-19    }
-20
-21    static String sanitize(String s) {
-22        StringBuilder sb = new StringBuilder();
-23        for (char c : s.toCharArray()) {
-24            if (Character.isLetterOrDigit(c)) {
-25                sb.append(Character.toLowerCase(c));
-26            }
-27        }
-28        return sb.toString();
-29    }
-30}
-31
-32// solution 1: using stack o(n)
-33// santize the string: convert to lowercase, remove all the non-alpha
-34// insert it into stack
-35// loop on stack and string pop and compare if != return fale
-36// return true
+8        while (end >= start) {
+9            if (ss.charAt(start++) != ss.charAt(end--)) {
+10                return false;
+11            }
+12        }
+13
+14        return true;
+15    }
+16
+17    static String sanitize(String s) {
+18        StringBuilder sb = new StringBuilder();
+19        for (char c : s.toCharArray()) {
+20            if (Character.isLetterOrDigit(c)) {
+21                sb.append(Character.toLowerCase(c));
+22            }
+23        }
+24        return sb.toString();
+25    }
+26}
+27
+28// solution 1: using stack o(n)
+29// sanitize the string: convert to lowercase, remove all the non-alpha
+30// insert it into stack
+31// loop on stack and string pop and compare if != return fale
+32// return true
+33
+34// solution 2: two pointers
+35// sanitize the string and iterate on it with two pointers: start & end
+36// if start != end return false
