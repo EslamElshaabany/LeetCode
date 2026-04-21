ee2dcaa537034e04dbf9cfa42b57d9389c01ad33
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-21 16:15:20 +0200

    [643] [Time Beats: 6.19%] [Memory Beats: 99.77%] - LeetPush

--- patterns-roadmap/Maximum-Average-Subarray-I.java
+++ patterns-roadmap/Maximum-Average-Subarray-I.java
@@ -3,31 +3,28 @@
 3
 4        int numsSize = nums.length;
 5
-6        int[] sumNums = new int[numsSize];
-7
-8        for (int i = 0; i < numsSize; i++) {
-9            if(i == 0) sumNums[i] = nums[i];
-10            else sumNums[i] = sumNums[i-1] + nums[i];    
-11        }
-12
-13        double maxSum = Double.NEGATIVE_INFINITY, currentSum = 0;
-14
-15        for (int i = k - 1; i < numsSize; i++) {
-16            if (i == k - 1) currentSum = sumNums[i];
-17            else currentSum = sumNums[i] - sumNums[i-k];
-18            maxSum =  Double.compare(currentSum, maxSum) > 0 ? currentSum : maxSum;
-19        }
-20        
-21        return maxSum / k;
-22    }
-23}
-24
-25// loop on the array 
-26// add the current elemnt to sum array
-27// 
-28
-29//[1,13,8,2,52,55]
-30// 2
-31// 51
-32// 42
-33
+6        Double bestSum = 0.0;
+7        for (int i = 0; i < k; i++) {
+8            bestSum += nums[i];  
+9        }
+10        Double currentWindow = bestSum;
+11        for (int i = k; i < numsSize; i++) {
+12            currentWindow += nums[i] - nums[i-k];
+13            bestSum =  Double.compare(currentWindow, bestSum) > 0 ? currentWindow : bestSum;
+14        }
+15        
+16        return bestSum / k;
+17    }
+18}
+19
+20// solution 1: o(n)
+21// loop on the array 
+22// add the current elemnt to sum array
+23// start form k as i and calc sum of k as sum[i] - sum[i-k]
+24// return max sum / k
+25
+26//[1,13,8,2,52,55]
+27// 2
+28// 51
+29// 42
+30
