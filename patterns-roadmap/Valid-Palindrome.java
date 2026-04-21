3e1c1860bbfaef0fa7581c3c94a1d0153b63a413
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-21 16:52:34 +0200

    [125] [Time Beats: 38.45%] [Memory Beats: 31.74%] - LeetPush

--- patterns-roadmap/Valid-Palindrome.java
+++ patterns-roadmap/Valid-Palindrome.java
@@ -10,25 +10,27 @@
 10        }
 11
 12        for (char c : ss.toCharArray()) {
-13            if(stack.pop() != c) return false;
-14        }
-15
-16        return true;
-17    }
-18
-19    static String sanitize(String s) {
-20        StringBuilder sb = new StringBuilder();
-21        for(char c : s.toCharArray()) {
-22            if (Character.isLetterOrDigit(c)) {
-23                sb.append(Character.toLowerCase(c));
-24            }
-25        }
-26        return sb.toString();
-27    }
-28}
-29
-30// solution 1: using stack o(n)
-31// santize the string: convert to lowercase, remove all the non-alpha
-32// insert it into stack
-33// loop on stack and string pop and compare if != return fale
-34// return true
+13            if (stack.pop() != c) {
+14                return false;
+15            }
+16        }
+17
+18        return true;
+19    }
+20
+21    static String sanitize(String s) {
+22        StringBuilder sb = new StringBuilder();
+23        for (char c : s.toCharArray()) {
+24            if (Character.isLetterOrDigit(c)) {
+25                sb.append(Character.toLowerCase(c));
+26            }
+27        }
+28        return sb.toString();
+29    }
+30}
+31
+32// solution 1: using stack o(n)
+33// santize the string: convert to lowercase, remove all the non-alpha
+34// insert it into stack
+35// loop on stack and string pop and compare if != return fale
+36// return true
