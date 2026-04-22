a0e29a1ffd8d9c0975453e4b3f473d6f5e20a9f8
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-22 15:21:05 +0200

    [392] [Time Beats: 70.81%] [Memory Beats: 48.24%] - LeetPush

--- patterns-roadmap/Is-Subsequence.java
+++ patterns-roadmap/Is-Subsequence.java
@@ -1,20 +1,17 @@
 1class Solution {
 2    public boolean isSubsequence(String s, String t) {
-3        String ss = new String(t);
-4        for(char c: s.toCharArray()) {
-5            int cIndex = ss.indexOf(c);
-6            if (cIndex == -1) {
-7                return false;
-8            } else {
-9                ss = ss.substring(cIndex+1, ss.length());
-10            }
-11        }
-12        return true;
-13    }
-14}
-15
-16// for every element in s 
-17// find it in t after this update the search sting to 
-18// be the substring after it
-19
-20// two pointers
+3        int i = 0, j = 0;
+4        while (i < s.length() && j < t.length()) {
+5            if (s.charAt(i) == t.charAt(j)) {
+6                i++;
+7            }
+8            j++;
+9        }
+10        return i == s.length();
+11    }
+12}
+13
+14// for every element in s 
+15// find it in t after this update the search sting to 
+16// be the substring after it
+17
