eea800cd20f63ee5a708a9cfe292eb8e90e858a4
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-24 01:31:22 +0300

    [121] [Time Beats: 39.20%] [Memory Beats: 17.20%] - LeetPush

--- patterns-roadmap/Best-Time-to-Buy-and-Sell-Stock.java
+++ patterns-roadmap/Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,26 +1,34 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        int maxProf = 0, minPrice = prices[0];
-4
-5        for (int i = 0; i < prices.length; i++) {
-6            minPrice = Math.min(prices[i], minPrice);
-7            maxProf = Math.max(prices[i] - minPrice, maxProf);
+3
+4        final int SIZE = prices.length;
+5        for (int i = 0; i < SIZE; i++) {
+6            if (i == SIZE-1) prices[i] = 0;
+7            else prices[i] = prices[i+1] - prices[i];
 8        }
 9
-10        return maxProf;
-11    }
-12}
-13
-14// solution 1: bruteforce o(n^2) TLE
-15// for ever element 
-16// loop on every element in next array
-17// update maxProf 
-18// return max Prof
-19
-20// solution 2: two pointers o(n)
-21// one pointer to track the min buy price (slow)
-22// other pointer update best profet (fast)
-23
-24// solution 3: sliding window
-25// covert the array to tomorro - today diff
-26// find the max profit by summing diffs rest max if sum < 0
+10        int maxProf = 0, currProf = 0;
+11
+12        for (int i = 0; i < SIZE; i++) {
+13            currProf += prices[i];
+14            maxProf = Math.max(maxProf, currProf);
+15            currProf = Math.max(currProf, 0);
+16        }
+17
+18        return maxProf;
+19    }
+20}
+21
+22// solution 1: bruteforce o(n^2) TLE
+23// for ever element 
+24// loop on every element in next array
+25// update maxProf 
+26// return max Prof
+27
+28// solution 2: two pointers o(n)
+29// one pointer to track the min buy price (slow)
+30// other pointer update best profet (fast)
+31
+32// solution 3: sliding window o(n)
+33// covert the array to tomorrow - today diff
+34// find the max profit by summing diffs rest max if sum < 0
