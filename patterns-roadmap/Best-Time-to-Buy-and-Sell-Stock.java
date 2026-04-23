a62a297e9f00eaa3fcd38dc4ed321e75d5177eb7
Eslam Elshaabany <eslam@elshaabany.me>
2026-04-24 01:03:46 +0300

    [121] [Time Beats: 99.92%] [Memory Beats: 72.19%] - LeetPush

--- patterns-roadmap/Best-Time-to-Buy-and-Sell-Stock.java
+++ patterns-roadmap/Best-Time-to-Buy-and-Sell-Stock.java
@@ -3,20 +3,24 @@
 3        int maxProf = 0, minPrice = prices[0];
 4
 5        for (int i = 0; i < prices.length; i++) {
-6            minPrice = prices[i] < minPrice ? prices[i] : minPrice;
-7            int prof = prices[i] - minPrice;
-8            maxProf = prof > maxProf ? prof : maxProf;
-9        }
-10
-11        return maxProf;
-12    }
-13}
-14
-15
-16// solution 1: bruteforce o(n^2)
-17// for ever element 
-18// loop on every element in next array
-19// update maxProf 
-20// return max Prof
-21
-22//
+6            minPrice = Math.min(prices[i], minPrice);
+7            maxProf = Math.max(prices[i] - minPrice, maxProf);
+8        }
+9
+10        return maxProf;
+11    }
+12}
+13
+14// solution 1: bruteforce o(n^2) TLE
+15// for ever element 
+16// loop on every element in next array
+17// update maxProf 
+18// return max Prof
+19
+20// solution 2: two pointers o(n)
+21// one pointer to track the min buy price (slow)
+22// other pointer update best profet (fast)
+23
+24// solution 3: sliding window
+25// covert the array to tomorro - today diff
+26// find the max profit by summing diffs rest max if sum < 0
