96f5f43b27ad6b7643725e08717be5383b2a730e
Eslam Elshaabany <eslam@elshaabany.me>
2026-06-27 00:47:46 +0300

    [347] [Time Beats: 20.09%] [Memory Beats: 10.75%] - LeetPush

--- patterns-roadmap/Top-K-Frequent-Elements.java
+++ patterns-roadmap/Top-K-Frequent-Elements.java
@@ -5,18 +5,31 @@
 5            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
 6        }
 7
-8        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(freqMap.entrySet());
-9        entryList.sort((a, b) -> Integer.compare(b.getValue(), a.getValue()));
-10
-11        int[] result = new int[k];
-12        for (int i = 0; i < k; i++) {
-13            result[i] = entryList.get(i).getKey();
-14        }
-15        return result;
-16    }
-17}
-18
-19// solution 1: using frequency map O(nlogn)
-20// insert elements into map with key = element, value = frequency O(n)
-21// sort the map by value O(nlogn)
-22// return the k top keys 
+8        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+9
+10        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+11            minHeap.offer(entry);
+12            if(minHeap.size() > k) {
+13                minHeap.poll();
+14            }
+15        }
+16
+17        int[] result = new int[k];
+18        for (int i = 0; i < k; i++) {
+19            result[i] = minHeap.poll().getKey();
+20        }
+21        return result;
+22    }
+23}
+24
+25// solution 1: using frequency map O(nlogn)
+26// insert elements into map with key = element, value = frequency O(n)
+27// sort the map by value O(nlogn)
+28// return the k top keys 
+29
+30// solution 2: using min heap O(nlogk)
+31// the priority queue of size k should have the most frequent elements
+32// insert elements into map with key = element, value = frequency O(n)
+33// insert the map entries in a periority queue of size k
+34// if the size of the queue > k poll
+35
