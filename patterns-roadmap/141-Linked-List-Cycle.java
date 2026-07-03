7588ed9cadb6b8de1b7b7c8fe8a052e0174b9cf1
Eslam Elshaabany <eslam@elshaabany.me>
2026-07-03 03:06:55 +0300

    [141] [Time Beats: 9.88%] [Memory Beats: 98.03%] - LeetPush

--- patterns-roadmap/141-Linked-List-Cycle.java
+++ patterns-roadmap/141-Linked-List-Cycle.java
@@ -11,10 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
-        if(head.val == Integer.MAX_VALUE) return true;
-        head.val = Integer.MAX_VALUE;
-        return hasCycle(head.next);
+        Set<ListNode> listMap = new HashSet<>();
+        while(head != null) {
+            if (listMap.contains(head)) return true;
+            listMap.add(head);
+            head = head.next;
+        }
+        return false;
     }
 }
 
@@ -23,3 +26,9 @@ public class Solution {
 // change every visited node value to Integer.MAX_VALUE
 // if you reached null return false 
 // if the current value is Integer.MAX_VALUE return true
+
+// solution 2: using map
+// assuming the every node has unique hash even with the same value
+// insert every node in the map 
+// if the node exist before return true 
+// return false
