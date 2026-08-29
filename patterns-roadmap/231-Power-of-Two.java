96537fdb62aeee9e48f5ba29568decbfa06f0aa2
Eslam Elshaabany <eslam@elshaabany.me>
2026-08-29 20:38:45 +0300

    [231] [Time Beats: 96.61%] [Memory Beats: 5.21%] - LeetPush

--- patterns-roadmap/231-Power-of-Two.java
+++ patterns-roadmap/231-Power-of-Two.java
@@ -1,20 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0)
-            return false;
-
-        return findOnes(n, 0) == 1;
-    }
-
-    int findOnes(int n, int ones) {
-        if (n == 0)
-            return ones;
-
-        ones += n % 2;
-
-        if (ones > 1)
-            return ones;
-
-        return findOnes(n / 2, ones);
+        return n > 0 && ((n & (n-1)) == 0);
     }
 }
