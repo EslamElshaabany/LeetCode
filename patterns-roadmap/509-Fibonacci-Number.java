e0a7e81bca6de2080d2ae0ce6673915064017301
Eslam Elshaabany <eslam@elshaabany.me>
2026-08-29 20:59:35 +0300

    [509] [Time Beats: 100.00%] [Memory Beats: 70.34%] - LeetPush

--- patterns-roadmap/509-Fibonacci-Number.java
+++ patterns-roadmap/509-Fibonacci-Number.java
@@ -1,7 +1,16 @@
 class Solution {
+
+    Map<Integer, Integer> fibMap = new HashMap<>();
+
     public int fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        return fib(n - 1) + fib(n - 2);
+        int fib_1 = fibMap.containsKey(n-1) ? fibMap.get(n-1) : fib(n-1);
+        int fib_2 = fibMap.containsKey(n-2) ? fibMap.get(n-2) : fib(n-2);
+
+        fibMap.put(n-1, fib_1);
+        fibMap.put(n-2, fib_2);
+
+        return fib_1 + fib_2;
     }
 }
